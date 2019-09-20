72f6f04f5b67727c5136f62a0a0bffe0183af3ed
Benson_Wu <benson_wu@mail.chungyo.net>
2019-09-20 17:08:24 +0800

    axios api

--- src/components/EmployeeApi.jsx
+++ src/components/EmployeeApi.jsx
@@ -1,13 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { readSync } from 'fs'
 
 const EmployeeApi = () => {
-    const [employee, setEmployee] = useState('')
+    const [loading_status, setLoadingStatus] = useState(false)
 
+    const getEmployeeData = () => {
+        document.getElementById('employee_data').innerHTML = "";
 
+        if (!loading_status) {
+            document.getElementById('employee_status').innerText = 'Loading...'
+        }
 
+        axios.get('http://dummy.restapiexample.com/api/v1/employees')
+            .then(res => {
+                let data = [...res.data]
+                data.map((value, index) => {
+                    console.log(value);
+                    let name_block = document.createElement('div');
+                    name_block.innerText = 'Dog Name ' + value.id + ': ' + value.employee_name;
+                    name_block.id = index
+                    document.getElementById('employee_data').appendChild(name_block)
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+            .finally(() => {
+                console.log('loading done');
+                document.getElementById('employee_status').innerText = 'Top is the result'
+            })
+    }
     return (
         <div>
-            123
+            <div id='employee_data' style={{ border: 'solid 2px rgba(0,0,0,.5)', padding: '8px 12px', width: '40%', margin: '20px auto', wordBreak: 'break-all' }}></div>
+            <div style={{ margin: '50px auto', width: '200px' }}>
+                <div id='employee_status'></div>
+                <button onClick={getEmployeeData} >Click To Get</button>
+            </div>
         </div>
     )
 }
